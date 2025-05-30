98bf78c7c78524308a1e60ca8daf842887fd9c71
Aziz Khasyi <azizkhasyi11@gmail.com>
2025-05-30 16:56:52 +0700

    feat: implement authentication and user management system
    - Added Prisma schema and migration for user, session, account, and verification tables.
    - Integrated Better Auth for email/password and social login (GitHub).
    - Created login form component with error handling and social login options.
    - Implemented theme provider for dark/light mode support.
    - Updated global styles with new color variables and responsive design.
    - Added utility functions for class name merging and styling.
    - Set up middleware for protected routes, redirecting unauthenticated users.
    - Introduced Toaster component for user notifications.

---
+++ src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+import prisma from "./prisma";
+import { username } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "sqlite",
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    nextCookies(),
+    username()
+  ]
+})
